c32436193a995612614e869910f14c34dd3fe732
Panagiotis Peikidis <pekalicious@gmail.com>
2026-02-04 02:50:28 +0200

    Added CORS for localhost

--- app/main.py
+++ app/main.py
@@ -3,12 +3,21 @@ from dotenv import load_dotenv
 load_dotenv(".env.local")
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from strawberry.fastapi import GraphQLRouter
 from app.graphql.schema import schema
 from app.routes.auth import router as auth_router
 from app.routes.users import router as users
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 graphql_app = GraphQLRouter(schema)
 
